0f8ae176fbd116494f7c5f6d3df8b13081d3dafd
anas.harby <anas.harby@gmail.com>
2018-11-30 03:27:17 +0200

    Initial mapping between GUI and game flow

--- src/main/java/controller/ViewController.java
+++ src/main/java/controller/ViewController.java
@@ -1,11 +1,16 @@
 package main.java.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import main.java.model.Agent;
 import main.java.model.agents.AggressiveAgent;
+import main.java.model.agents.GreedyAgent;
+import main.java.model.agents.HumanAgent;
 import main.java.model.agents.PassiveAgent;
 import main.java.model.game.Game;
 import main.java.model.game.GameState;
@@ -18,18 +23,23 @@ public class ViewController {
     /** FXML Variables **/
     @FXML private StackPane root;
     @FXML private BorderPane gameOverPane;
+    @FXML private JFXButton attackButton;
+    @FXML private JFXButton nextTurnButton;
+    @FXML private ImageView curTurnIcon;
 
     /** Game Variables **/
     private Game game;
-    private Agent agent1, agent2;
+    private Agent player1, player2;
     private GameState curGameState;
 
     /** GraphStream Variables **/
     private GraphView graphView;
 
-    /** Constant Variables **/
+    /** Constant Variables (assets) **/
     private static final String STYLE_SHEET_PATH = "main/resources/view/css/graph.css";
     private static final String BACKGROUND_PATH = "src/main/resources/view/assets/world_1200_800.png";
+    private static final String PLAYER_1_ICON = "main/resources/view/assets/circle_1.png";
+    private static final String PLAYER_2_ICON = "main/resources/view/assets/circle_2.png";
 
     /** Testing Variables **/
     private final static String FILE_NAME = "./risk_game.txt";
@@ -38,14 +48,14 @@ public class ViewController {
     public void initialize() {
         this.initGame();
         this.initGraphStream();
+        this.initControls();
     }
 
     private void initGame() {
         this.game = Game.getInstance();
-        this.agent1 = new AggressiveAgent();
-        this.agent2 = new PassiveAgent();
+        this.player1 = new PassiveAgent();
+        this.player2 = new PassiveAgent();
         this.curGameState = new GameState(FILE_NAME);
-
     }
 
     private void initGraphStream() {
@@ -78,5 +88,27 @@ public class ViewController {
                 System.out.println("Button released on node: " + id);
             }
         });
+    }
+
+    private void initControls() {
+        boolean human = this.player1 instanceof HumanAgent;
+        this.attackButton.setDisable(!human);
+        this.nextTurnButton.setDisable(human);
+    }
+
+    @FXML
+    public void nextTurn() {
+        this.game.play(this.curGameState, player1, player2);
+        this.toggleTurns();
+        if (this.curGameState.isFinalState()) {
+            this.nextTurnButton.setDisable(true);
+//            this.drawGameOver();
+        }
+    }
+
+    private void toggleTurns() {
+        this.curTurnIcon.setImage(new Image(
+                this.curGameState.getCurrentPlayer().equals(this.curGameState.getWorld().getPlayerOne()) ?
+                        PLAYER_1_ICON : PLAYER_2_ICON));
     }
 }
--- src/main/java/model/game/Game.java
+++ src/main/java/model/game/Game.java
@@ -19,7 +19,7 @@ public class Game {
         // Play takes both players' agents and plays the game starting from the given game state
         GameState currGameState = initialState;
         int cnt = 0;
-        while (!currGameState.isFinalState()) {
+//        while (!currGameState.isFinalState()) {
             if (currGameState.getCurrentPlayer().equals(currGameState.getWorld().getPlayerOne())) {
                 currGameState = playerOneAgent.getNextState(currGameState);
             } else {
@@ -30,12 +30,12 @@ public class Game {
                 System.out.print(c.getUnits() + " ");
             }
             System.out.println();
-        }
-        System.out.println("Winner is player with Id: " + currGameState.getWinner().getId());
-        System.out.println();
-        System.out.println("===============================================================================");
-        System.out.println("Final game state: ");
-        System.out.println("===============================================================================");
-        System.out.println(currGameState.toString());
+//        }
+//        System.out.println("Winner is player with Id: " + currGameState.getWinner().getId());
+//        System.out.println();
+//        System.out.println("===============================================================================");
+//        System.out.println("Final game state: ");
+//        System.out.println("===============================================================================");
+//        System.out.println(currGameState.toString());
     }
 }
